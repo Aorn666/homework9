8b35ceb97eb54baf651f2ede59d668cd6a01d648
Ilya Matveev01~ <aorn666@gmail.com>
2022-11-16 20:34:22 +0300

    всё

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int ResultNumbers(int a, int b)
+{
+if (b == a)
+return a;
+return ResultNumbers(a, b -1 ) + b;
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ResultNumbers(a, b));
---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+
+int ResultNumbers(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return ResultNumbers (m - 1, 1);
+    else
+        return ResultNumbers(m - 1, ResultNumbers(m, n - 1)); ;
+}
+
+
+Console.Clear();
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("A(m,n):" + ResultNumbers(n, m));      
